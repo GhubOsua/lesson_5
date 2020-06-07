006747b5fb0e50b679aa0afafa3ff55b9dc84dca
Alex <64502957+GhubOsua@users.noreply.github.com>
2020-06-08 03:14:02 +0500

    Update script_serv.sh

--- script_serv.sh
+++ script_serv.sh
@@ -6,21 +6,33 @@ sleep 5
 echo "Готово!!!!Урааа!!!"
 echo "==========================================================================="
 
-echo "Включение службп rpc nfs"
+echo "Включаем автозагрузку для служб rpc nfs"
 systemctl enable rpcbind nfs-server
 sleep 5
 echo "Готово!!!!Урааа!!!"
 echo "==========================================================================="
 
-echo "Выполняется запуск служб rpcbind nfs-server"
-systemctl start rpcbind nfs-server
-sleep 5
-echo "Готово!!!!Урааа!!!"
+echo "Работа с firewalld"
+systemctl enable firewalld.service
+systemctl start firewalld.service
+firewall-cmd --zone=public --add-port=111/udp --permanent
+firewall-cmd --zone=public --add-port=20048/udp --permanent
+firewall-cmd --zone=public --add-port=47357/udp --permanent
+firewall-cmd --zone=public --add-port=38743/udp --permanent
+firewall-cmd --zone=public --add-port=2049/udp --permanent
+firewall-cmd --reload
 
+#firewall-cmd --permanent --zone=public --add-service=nfs
+#firewall-cmd --permanent --zone=public --add-service=mountd
+#firewall-cmd --permanent --zone=public --add-service=rpc-bind
+#firewall-cmd --reload
+echo "Готово!!!!Урааа!!!"
 echo "==========================================================================="
+
 echo "Создание папки для NFS"
 mkdir /nfs_share
 chmod -R 777 /nfs_share
+echo "Готово!!!!Урааа!!!"
 echo "==========================================================================="
 
 echo "Настройка файла exports"
@@ -30,17 +42,21 @@ echo "==========================================================================
 
 echo "Перезапуск службы nfs, чтобы служба перечитала конфигурацию"
 exportfs -r
-echo "==========================================================================="
-
-echo "Работа с firewalld"
-systemctl enable firewalld.service
-systemctl start firewalld.service
-firewall-cmd --permanent --zone=public --add-service=nfs
-firewall-cmd --permanent --zone=public --add-service=mountd
-firewall-cmd --permanent --zone=public --add-service=rpc-bind
-firewall-cmd --reload
 echo "Готово!!!!Урааа!!!"
 echo "==========================================================================="
+
+echo "Устанавливаем конкретные порты NFS для firewall"
+echo -e "RPCNFSDARGS="--port 2049" \nMOUNTD_PORT=20048 \nLOCKD_TCPPORT=47357 \nLOCKD_UDPPORT=47357 \nSTATD_PORT=38743"
+echo "Готово!!!!Урааа!!!"
+echo "==========================================================================="
+
+echo "Выполняется запуск служб rpcbind nfs-server"
+systemctl start rpcbind nfs-server
+sleep 5
+echo "Готово!!!!Урааа!!!"
+echo "==========================================================================="
+
+
 
 #####
 1) 111/udp 20048/udp 47357/udp 38743/udp 2049/udp
